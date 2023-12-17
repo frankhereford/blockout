70a26b4bf112878ae0ef7d35f620c9e4b7cc13d8
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-17 13:05:03 -0600

    Add origin parameter to ElPiece component

--- web/src/pages/Components/pieces/elPiece.tsx
+++ web/src/pages/Components/pieces/elPiece.tsx
@@ -4,14 +4,15 @@ import { Cube } from '../Cube';
 interface ElPieceProps {
     offset: Vector3;
     rotation?: Vector3;
+    origin?: Vector3;
 }
 
-export const ElPiece = ({ offset, rotation = new Vector3(0, 0, 0) }: ElPieceProps) => {
+export const ElPiece = ({ offset, rotation = new Vector3(0, 0, 0), origin = new Vector3(1, 0, 0) }: ElPieceProps) => {
     const cubes = [
         new Vector3(0, 0, 0),
         new Vector3(1, 0, 0),
         new Vector3(2, 0, 0),
-    ].map(cube => cube.clone().applyEuler(new Euler(rotation.x, rotation.y, rotation.z)));
+    ].map(cube => cube.clone().sub(origin).applyEuler(new Euler(rotation.x, rotation.y, rotation.z)).add(origin));
 
     return (
         <>
