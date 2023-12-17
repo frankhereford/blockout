4e8215e6166898fa42eb66476e9179390316a698
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-17 10:44:31 -0600

    Refactor camera orbiting logic

--- web/src/pages/Components/Camera.tsx
+++ web/src/pages/Components/Camera.tsx
@@ -4,35 +4,55 @@ import { OrbitControls } from "@react-three/drei";
 
 export const Camera = () => {
     const { camera } = useThree();
-    const clockRef = useRef({ elapsedTime: 0 });
-    const [isOrbiting, setIsOrbiting] = useState(false);
+    const [isManualOrbiting, setIsManualOrbiting] = useState(false);
+    const [cameraHeight, setCameraHeight] = useState(0);
+    const [cameraOrbitRadius, setCameraOrbitRadius] = useState(5);
+    const [angle, setAngle] = useState(0);
+    const [orbitSpeed, setOrbitSpeed] = useState(0.1); // Adjust this value to change the speed of orbiting
 
     useEffect(() => {
         camera.position.y = 10; // Set initial height of the camera
-        camera.lookAt(2.5, 0, 2.5);
+        setCameraHeight(camera.position.y);
     }, [camera]);
 
+    const calculateOrbitRadius = () => {
+        const dx = 2.5 - camera.position.x;
+        const dz = 2.5 - camera.position.z;
+        const distance = Math.sqrt(dx*dx + dz*dz);
+        setCameraOrbitRadius(distance);
+    };
     useFrame(() => {
-        if (!isOrbiting) {
-            const clock = clockRef.current;
-            clock.elapsedTime += 0.001; // Adjust this value to control the speed of the orbit
+        camera.lookAt(2.5, 0, 2.5);
 
-            const radius = 2;
-            const centerX = 2.5;
-            const centerZ = 2.5;
-
-            // Circular motion on the x-z plane
-            camera.position.x = centerX + (radius * Math.cos(clock.elapsedTime));
-            camera.position.z = centerZ + (radius * Math.sin(clock.elapsedTime));
-            camera.lookAt(2.5, 0, 2.5); // Keep the camera looking at the center
+        if (!isManualOrbiting) {
+            const radian = angle * (Math.PI / 180);
+            camera.position.x = 2.5 + cameraOrbitRadius * Math.cos(radian);
+            camera.position.z = 2.5 + cameraOrbitRadius * Math.sin(radian);
+            setAngle(prevAngle => (prevAngle + orbitSpeed) % 360);
         }
     });
 
     return (
         <>
             <OrbitControls
-                onStart={() => setIsOrbiting(true)}
-                onEnd={() => setIsOrbiting(false)}
+                onStart={() => {
+                    setIsManualOrbiting(true);
+                    console.log('Orbiting engaged');
+                }}
+                onEnd={() => {
+                    setIsManualOrbiting(false);
+                    console.log('Orbiting disengaged');
+                    console.log(`Camera position: x=${camera.position.x}, y=${camera.position.y}, z=${camera.position.z}`);
+                    calculateOrbitRadius();
+                    console.log(`Camera orbit radius: ${cameraOrbitRadius}`);
+
+                    // Calculate the correct angle
+                    const dx = camera.position.x - 2.5;
+                    const dz = camera.position.z - 2.5;
+                    const radian = Math.atan2(dz, dx);
+                    const newAngle = radian * (180 / Math.PI);
+                    setAngle(newAngle);
+                }}
             />
         </>
     )
