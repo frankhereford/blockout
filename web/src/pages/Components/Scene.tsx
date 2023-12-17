9d37e03d2efc562ab37bfc4887f07f316dae8e0c
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-17 11:44:02 -0600

    Refactor Scene component to accept width, height, and depth as props

--- web/src/pages/Components/Scene.tsx
+++ web/src/pages/Components/Scene.tsx
@@ -5,15 +5,21 @@ import { Lighting } from "./Lighting";
 import { AxesLabels } from "./AxesLabels";
 import { GroundPlane } from "./GroundPlane";
 
-export const Scene = () => {
+interface SceneProps {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export const Scene = ({width, height, depth}: SceneProps) => {
     return (
         <>
-            <Camera />
+            <Camera width={width} height={height} depth={depth} />
             <Cube />
-            <Well width={5} height={3} depth={5} />
-            <AxesLabels />
-            <Lighting />
-            <GroundPlane />
+            <Well width={width} height={height} depth={depth} />
+            <AxesLabels width={width} height={height} depth={depth} />
+            <Lighting width={width} height={height} depth={depth} />
+            <GroundPlane width={width} height={height} depth={depth} />
         </>
     );
 };
