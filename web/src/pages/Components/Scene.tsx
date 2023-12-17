ed34d011ff445c191804ff6386c6f0d5d3a33136
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-16 21:44:31 -0600

    Update camera position and lookAt in Scene component

--- web/src/pages/Components/Scene.tsx
+++ web/src/pages/Components/Scene.tsx
@@ -1,13 +1,24 @@
 //import { extend } from '@react-three/fiber'
+import { useEffect } from 'react';
+import { useThree } from '@react-three/fiber';
 import { OrbitControls  } from "@react-three/drei";
 import Grid from "./Grid";
 
 //extend({ OrbitControls })
 
 export const Scene = () => {
+    const { camera } = useThree();
+
+    useEffect(() => {
+        camera.position.x = 2.5;
+        camera.position.y = 10;
+        camera.position.z = 2.5;
+        camera.lookAt(2.5, 0, 2.5);
+    }, [camera]);
+
+            //<OrbitControls />
     return (
         <>
-            <OrbitControls />
             <mesh position={[.5, .5, .5]}>
                 <boxGeometry args={[1, 1, 1]} />
                 <meshNormalMaterial />
