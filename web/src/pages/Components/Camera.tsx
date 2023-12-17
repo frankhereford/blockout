034cc2943e78c2b1d813072cf76671ee1bbcebfb
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-17 10:13:38 -0600

    Add orbiting functionality to Camera component

--- web/src/pages/Components/Camera.tsx
+++ web/src/pages/Components/Camera.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
 import { OrbitControls } from "@react-three/drei";
-
 
 export const Camera = () => {
     const { camera } = useThree();
     const clockRef = useRef({ elapsedTime: 0 });
+    const [isOrbiting, setIsOrbiting] = useState(false);
 
     useEffect(() => {
         camera.position.y = 10; // Set initial height of the camera
@@ -13,23 +13,27 @@ export const Camera = () => {
     }, [camera]);
 
     useFrame(() => {
-        const clock = clockRef.current;
-        clock.elapsedTime += 0.001; // Adjust this value to control the speed of the orbit
+        if (!isOrbiting) {
+            const clock = clockRef.current;
+            clock.elapsedTime += 0.001; // Adjust this value to control the speed of the orbit
 
-        const radius = 2;
-        const centerX = 2.5;
-        const centerZ = 2.5;
+            const radius = 2;
+            const centerX = 2.5;
+            const centerZ = 2.5;
 
-        // Circular motion on the x-z plane
-        camera.position.x = centerX + (radius * Math.cos(clock.elapsedTime));
-        camera.position.z = centerZ + (radius * Math.sin(clock.elapsedTime));
-        camera.lookAt(2.5, 0, 2.5); // Keep the camera looking at the center
+            // Circular motion on the x-z plane
+            camera.position.x = centerX + (radius * Math.cos(clock.elapsedTime));
+            camera.position.z = centerZ + (radius * Math.sin(clock.elapsedTime));
+            camera.lookAt(2.5, 0, 2.5); // Keep the camera looking at the center
+        }
     });
 
-    //return null;
     return (
         <>
-            <OrbitControls />
+            <OrbitControls
+                onStart={() => setIsOrbiting(true)}
+                onEnd={() => setIsOrbiting(false)}
+            />
         </>
     )
 };
