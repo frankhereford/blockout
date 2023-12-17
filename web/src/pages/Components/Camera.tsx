92f8c84183212e6609c7466da25b7fe511ef60e3
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-17 11:53:51 -0600

    Update Camera component with dynamic width and depth

--- web/src/pages/Components/Camera.tsx
+++ web/src/pages/Components/Camera.tsx
@@ -2,32 +2,39 @@ import { useEffect, useRef, useState } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
 import { OrbitControls } from "@react-three/drei";
 
-export const Camera = () => {
+interface CameraProps {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export const Camera = ({width, height, depth}: CameraProps) => {
     const { camera } = useThree();
     const [isManualOrbiting, setIsManualOrbiting] = useState(false);
-    const [cameraHeight, setCameraHeight] = useState(0);
-    const [cameraOrbitRadius, setCameraOrbitRadius] = useState(5);
+    const [cameraHeight, setCameraHeight] = useState(height);
+    const [cameraOrbitRadius, setCameraOrbitRadius] = useState(1);
     const [angle, setAngle] = useState(0);
     const [orbitSpeed, setOrbitSpeed] = useState(0.1); // Adjust this value to change the speed of orbiting
 
     useEffect(() => {
-        camera.position.y = 10; // Set initial height of the camera
+        camera.position.y = 1.8 * height; // Set initial height of the camera
         setCameraHeight(camera.position.y);
-    }, [camera]);
+    }, [camera, height]);
 
     const calculateOrbitRadius = () => {
-        const dx = 2.5 - camera.position.x;
-        const dz = 2.5 - camera.position.z;
+        const dx = width / 2 - camera.position.x;
+        const dz = depth / 2 - camera.position.z;
         const distance = Math.sqrt(dx*dx + dz*dz);
         setCameraOrbitRadius(distance);
     };
+
     useFrame(() => {
-        camera.lookAt(2.5, 0, 2.5);
+        camera.lookAt(width / 2, 0, depth / 2);
 
         if (!isManualOrbiting) {
             const radian = angle * (Math.PI / 180);
-            camera.position.x = 2.5 + cameraOrbitRadius * Math.cos(radian);
-            camera.position.z = 2.5 + cameraOrbitRadius * Math.sin(radian);
+            camera.position.x = width / 2 + cameraOrbitRadius * Math.cos(radian);
+            camera.position.z = depth / 2 + cameraOrbitRadius * Math.sin(radian);
             setAngle(prevAngle => (prevAngle + orbitSpeed) % 360);
         }
     });
@@ -47,8 +54,8 @@ export const Camera = () => {
                     console.log(`Camera orbit radius: ${cameraOrbitRadius}`);
 
                     // Calculate the correct angle
-                    const dx = camera.position.x - 2.5;
-                    const dz = camera.position.z - 2.5;
+                    const dx = camera.position.x - width / 2;
+                    const dz = camera.position.z - depth / 2;
                     const radian = Math.atan2(dz, dx);
                     const newAngle = radian * (180 / Math.PI);
                     setAngle(newAngle);
