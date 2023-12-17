70a1ce8ce15f1727478154a139c35a5cbc85708e
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-17 11:43:56 -0600

    Refactor SpotLight component and add props

--- web/src/pages/Components/Lighting.tsx
+++ web/src/pages/Components/Lighting.tsx
@@ -2,26 +2,38 @@ import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import type { SpotLight as ThreeSpotLight } from 'three'; // Import SpotLight from three package with a different name
 
-function SpotLight() {
+interface SpotLightProps {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+function SpotLight({width, height, depth}: SpotLightProps) {
     const spotLightRef = useRef<ThreeSpotLight>(null); // Assign SpotLight type to spotLightRef
 
     useFrame(() => {
         if (spotLightRef.current) {
-            spotLightRef.current.target.position.set(2.5, 0, 2.5); // Replace x, y, z with the coordinates you want to target
+            spotLightRef.current.target.position.set(width / 2, 0, depth / 2); // Replace x, y, z with the coordinates you want to target
             spotLightRef.current.target.updateMatrixWorld(); // Necessary to apply the changes
         }
     });
 
     return (
-        <spotLight ref={spotLightRef} position={[2.5, 8, 2.5]} angle={1} penumbra={1} intensity={500} castShadow shadow-mapSize-width={2048} shadow-mapSize-height={2048} />
+        <spotLight ref={spotLightRef} position={[width / 2, height + 3, depth / 2]} angle={1} penumbra={.5} intensity={300} castShadow shadow-mapSize-width={2048} shadow-mapSize-height={2048} />
     );
 }
 
-export function Lighting() {
+interface LightingProps {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export function Lighting({width, height, depth}: LightingProps) {
     return (
         <>
             <ambientLight intensity={.3} />
-            <SpotLight />
+            <SpotLight width={width} height={height} depth={depth} />
         </>
     );
 }
