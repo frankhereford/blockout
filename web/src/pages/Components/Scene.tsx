5d42dbbcdb3e67aebf5d095e6f180ed00aac4a0f
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-16 23:30:45 -0600

    moving block

--- web/src/pages/Components/Scene.tsx
+++ web/src/pages/Components/Scene.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
 import { OrbitControls  } from "@react-three/drei";
 import Grid from "./Grid";
 
-//extend({ OrbitControls })
 export const Scene = () => {
 
     const { camera } = useThree();
     const clockRef = useRef({ elapsedTime: 0 });
+    const [cubePosition, setCubePosition] = useState(4.5); // Initial cube position
 
     useEffect(() => {
         camera.position.y = 10; // Set initial height of the camera
@@ -25,14 +25,18 @@ export const Scene = () => {
         // Circular motion on the x-z plane
         camera.position.x = centerX + (radius * Math.cos(clock.elapsedTime));
         camera.position.z = centerZ + (radius * Math.sin(clock.elapsedTime));
+        let seconds = Math.floor(clock.elapsedTime * 10);
+        let height = 5 - (seconds % 5);
+        console.log(height)
+        setCubePosition(height - .5); // Update cube position using state setter function
 
         camera.lookAt(2.5, 0, 2.5); // Keep the camera looking at the center
-    });
 
+    });
 
     return (
         <>
-            <mesh position={[0.5, 0.5, 0.5]} castShadow>
+            <mesh position={[0.5, cubePosition, 0.5]} castShadow>
                 <boxGeometry args={[1, 1, 1]} />
                 <meshNormalMaterial />
             </mesh>
