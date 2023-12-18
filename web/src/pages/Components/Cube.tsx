54eb44fb29f55d9fa383825bbe159336a524b5d9
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-18 09:14:59 -0600

    Update Cube and Lighting components

--- web/src/pages/Components/Cube.tsx
+++ web/src/pages/Components/Cube.tsx
@@ -9,9 +9,9 @@ export const Cube = ({location, scale = 0.97}: CubeProps) => {
     const offsetLocation = new Vector3(location.x + 0.5, location.y + 0.5, location.z + 0.5);
 
     return (
-        <mesh position={offsetLocation} scale={[scale, scale, scale]} castShadow>
+        <mesh position={offsetLocation} scale={[scale, scale, scale]} castShadow receiveShadow>
             <boxGeometry args={[1, 1, 1]} />
-            <meshNormalMaterial />
+            <meshStandardMaterial attach="material" color="red" />
         </mesh>
     );
 };
--- web/src/pages/Components/Lighting.tsx
+++ web/src/pages/Components/Lighting.tsx
@@ -8,14 +8,15 @@ interface LightingProps {
 }
 
 export function Lighting({width, height, depth}: LightingProps) {
+    const lightLift = 5;
     return (
         <>
             <ambientLight intensity={.4} />
-            <SpotLight intensity={300} width={width} height={height} depth={depth} position={[width / 2, height + 3, depth / 2]} />
-            <SpotLight intensity={50} width={width} height={height} depth={depth} position={[width, height + 3, depth]} />
-            <SpotLight intensity={50} width={width} height={height} depth={depth} position={[0, height + 3, 0]} />
-            <SpotLight intensity={50} width={width} height={height} depth={depth} position={[width, height + 3, 0]} />
-            <SpotLight intensity={50} width={width} height={height} depth={depth} position={[0, height + 3, depth]} />
+            <SpotLight intensity={100} width={width} height={height} depth={depth} position={[width / 2, height + lightLift, depth / 2]} />
+            <SpotLight intensity={100} width={width} height={height} depth={depth} position={[width, height + lightLift, depth]} />
+            <SpotLight intensity={100} width={width} height={height} depth={depth} position={[0, height + lightLift, 0]} />
+            <SpotLight intensity={100} width={width} height={height} depth={depth} position={[width, height + lightLift, 0]} />
+            <SpotLight intensity={100} width={width} height={height} depth={depth} position={[0, height + lightLift, depth]} />
         </>
     );
 }
