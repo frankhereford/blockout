845c50fc8c29c57f8d31859ba8b4648dafd9a3d5
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-18 06:42:24 -0600

    Refactor Lighting component and add SpotLight module

---
+++ web/src/pages/Components/lights/SpotLight.tsx
@@ -0,0 +1,24 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import type { SpotLight as ThreeSpotLight } from 'three'; // Import SpotLight from three package with a different name
+
+interface SpotLightProps {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export function SpotLight({ width, height, depth }: SpotLightProps) {
+    const spotLightRef = useRef<ThreeSpotLight>(null);
+
+    useFrame(() => {
+        if (spotLightRef.current) {
+            spotLightRef.current.target.position.set(width / 2, 0, depth / 2);
+            spotLightRef.current.target.updateMatrixWorld();
+        }
+    });
+
+    return (
+        <spotLight ref={spotLightRef} position={[width / 2, height + 3, depth / 2]} angle={1} penumbra={.5} intensity={300} castShadow shadow-mapSize-width={2048} shadow-mapSize-height={2048} />
+    );
+}
