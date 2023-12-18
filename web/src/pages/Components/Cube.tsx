aaaf5ab579feb44d1beb874b15213873292fa0cf
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-18 13:02:21 -0600

    Update Cube component props

--- web/src/pages/Components/Cube.tsx
+++ web/src/pages/Components/Cube.tsx
@@ -6,13 +6,15 @@ interface CubeProps {
     color?: string;
 }
 
-export const Cube = ({location, scale = 0.87, color = "red"}: CubeProps) => {
+export const Cube = ({location, scale = 0.93, color = "red"}: CubeProps) => {
     const offsetLocation = new Vector3(location.x + 0.5, location.y + 0.5, location.z + 0.5);
 
     return (
         <mesh position={offsetLocation} scale={[scale, scale, scale]} castShadow receiveShadow>
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial attach="material" color={color} />
+            {/* <meshStandardMaterial attach="material" color={color} /> */}
+            {/* <meshPhongMaterial attach="material" color={color} specular="#FFFFFF" shininess={50} /> */}
+            <meshPhysicalMaterial attach="material" color={color} clearcoat={1} clearcoatRoughness={1} roughness={0} metalness={.5} />
         </mesh>
     );
 };
