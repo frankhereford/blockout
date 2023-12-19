3c58e2ff23b89cc192cb9ded588424b8c78ec5f9
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-19 06:24:22 -0600

    Add Piece types and coordinates

--- web/src/pages/Components/Piece.tsx
+++ web/src/pages/Components/Piece.tsx
@@ -1,6 +1,60 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+import { Cube } from './Cube';
+
+type PieceType = 'el' | 'tee' | 'block' | 'solo';
+
 interface PieceProps {
+    piece: PieceType;
 }
+
+interface Coordinate {
+    x: number;
+    y: number;
+    z: number;
+}
+
+interface Piece {
+    coordinates: Coordinate[];
+    origin: Coordinate;
+}
+
+const pieces: Record<PieceType, Piece> = {
+    'el': {
+        coordinates: [
+            { x: 0, y: 0, z: 0 },
+            { x: 1, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 1, y: 1, z: 0 }
+        ],
+        origin: { x: 1, y: 0, z: 0 }
+    },
+    'tee': {
+        coordinates: [
+            { x: 0, y: 0, z: 0 },
+            { x: 1, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 1, y: 1, z: 0 }
+        ],
+        origin: { x: 1, y: 0, z: 0 }
+    },
+    'block': {
+        coordinates: [
+            { x: 0, y: 0, z: 0 },
+            { x: 1, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 1, y: 1, z: 0 }
+        ],
+        origin: { x: 1, y: 0, z: 0 }
+    },
+    'solo': {
+        coordinates: [
+            { x: 0, y: 0, z: 0 },
+            { x: 1, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 1, y: 1, z: 0 }
+        ],
+        origin: { x: 1, y: 0, z: 0 }
+    }
+};
 
 export const Piece = ({ }: PieceProps) => {
     return (
