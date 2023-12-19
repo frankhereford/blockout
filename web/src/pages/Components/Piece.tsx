dbe1e0c0348bafb71310a2814d990f4991b32d17
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-19 08:02:11 -0600

    animation working

--- web/src/pages/Components/Piece.tsx
+++ web/src/pages/Components/Piece.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 //import { Cube } from './Cube';
 import { pieces } from './pieces';
 import type { PieceType } from './pieces';
@@ -6,23 +9,22 @@ import { useSpring, animated } from '@react-spring/three'
 
 
 interface CubeProps {
-    location: Vector3;
+    location: AnimatedValue<Vector3>;
     scale?: number;
     color?: string;
 }
 
 export const Cube = ({ location, scale = 0.93, color = "red" }: CubeProps) => {
-    const offsetLocation = new Vector3(location.x + 0.5, location.y + 0.5, location.z + 0.5);
+    const offsetLocation = location.to((x, y, z) => new Vector3(x + 0.5, y + 0.5, z + 0.5));
 
     return (
         <animated.mesh position={offsetLocation} scale={[scale, scale, scale]} castShadow receiveShadow>
             <boxGeometry args={[1, 1, 1]} />
-            {/* <meshStandardMaterial attach="material" color={color} /> */}
-            {/* <meshPhongMaterial attach="material" color={color} specular="#FFFFFF" shininess={50} /> */}
             <meshPhysicalMaterial attach="material" color={color} clearcoat={1} clearcoatRoughness={1} roughness={0} metalness={.5} />
         </animated.mesh>
     );
 };
+
 
 
 interface PieceProps {
@@ -32,24 +34,24 @@ interface PieceProps {
 }
 
 export const Piece = ({ piece = 'tee', location = new Vector3(0, 0, 0), rotation = new Vector3(0, 0, 0) }: PieceProps) => {
+    const { coordinates, color, origin } = pieces[piece];
+    const rotation_unit = Math.PI / 2;
+    const eulerRotation = new Euler(rotation.x * rotation_unit, rotation.y * rotation_unit, rotation.z * rotation_unit);
 
-    const createCubes = (pieceName: PieceType) => {
-        const { coordinates, color, origin } = pieces[pieceName];
-        const rotation_unit = Math.PI / 2;
-        const eulerRotation = new Euler(rotation.x * rotation_unit, rotation.y * rotation_unit, rotation.z * rotation_unit);
-        return coordinates.map((coordinate, index) => {
-            const offsetCoordinate = coordinate.clone().sub(origin);
-            offsetCoordinate.applyEuler(eulerRotation);
-            offsetCoordinate.add(origin);
-            offsetCoordinate.add(location);
-            return <Cube key={index} location={offsetCoordinate} color={color} />;
+    const createCubes = (coordinate: Vector3, index: number) => {
+        const offsetCoordinate = coordinate.clone().sub(origin);
+        offsetCoordinate.applyEuler(eulerRotation);
+        offsetCoordinate.add(origin);
+        const spring = useSpring({
+            location: [offsetCoordinate.x + location.x, offsetCoordinate.y + location.y, offsetCoordinate.z + location.z],
+            config: { mass: 1, tension: 170, friction: 26 }, // You can adjust these values for different animation effects
         });
+        return <Cube key={index} location={spring.location} color={color} />;
     };
 
     return (
         <>
-            {createCubes(piece)} 
+            {coordinates.map(createCubes)}
         </>
     );
-
 };
