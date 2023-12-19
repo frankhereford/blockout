ac4f87a4abe39c7c07baa76a89492ecd14e654c1
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-19 06:06:16 -0600

    Add Piece component to Scene

---
+++ web/src/pages/Components/Piece.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface PieceProps {
+}
+
+export const Piece = ({ }: PieceProps) => {
+    return (
+        <>
+        </>
+    );
+};
