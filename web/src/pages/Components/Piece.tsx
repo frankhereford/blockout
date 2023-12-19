57e0a334731674553100f3a971afdfffea7a1427
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-19 07:41:27 -0600

    pull cube in here for easier context observtion

--- web/src/pages/Components/Piece.tsx
+++ web/src/pages/Components/Piece.tsx
@@ -1,7 +1,29 @@
-import { Cube } from './Cube';
+//import { Cube } from './Cube';
 import { pieces } from './pieces';
 import type { PieceType } from './pieces';
 import { Vector3, Euler } from 'three';
+import { useSpring, animated } from '@react-spring/three'
+
+
+interface CubeProps {
+    location: Vector3;
+    scale?: number;
+    color?: string;
+}
+
+export const Cube = ({ location, scale = 0.93, color = "red" }: CubeProps) => {
+    const offsetLocation = new Vector3(location.x + 0.5, location.y + 0.5, location.z + 0.5);
+
+    return (
+        <animated.mesh position={offsetLocation} scale={[scale, scale, scale]} castShadow receiveShadow>
+            <boxGeometry args={[1, 1, 1]} />
+            {/* <meshStandardMaterial attach="material" color={color} /> */}
+            {/* <meshPhongMaterial attach="material" color={color} specular="#FFFFFF" shininess={50} /> */}
+            <meshPhysicalMaterial attach="material" color={color} clearcoat={1} clearcoatRoughness={1} roughness={0} metalness={.5} />
+        </animated.mesh>
+    );
+};
+
 
 interface PieceProps {
     piece?: PieceType;
