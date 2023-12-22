dbce11c9c6fbe9b3bbf80e0e75837ac10cba1442
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 18:44:59 -0600

    Commented out postRouter and added gameRouter to appRouter

--- ui/src/server/api/root.ts
+++ ui/src/server/api/root.ts
@@ -1,4 +1,5 @@
-import { postRouter } from "~/server/api/routers/post";
+//import { postRouter } from "~/server/api/routers/post";
+import { gameRouter } from "~/server/api/routers/game";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +8,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  game: gameRouter,
 });
 
 // export type definition of API
