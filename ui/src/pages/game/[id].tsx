cae27d67ffa336438ad03098776a4604662a6ed7
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 20:05:32 -0600

    Add game page component

---
+++ ui/src/pages/game/[id].tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+import { api } from "~/utils/api";
+
+export default function Game() {
+
+    return (
+        <>
+            <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#89e3fc] to-[#3a3e89]">
+                this is to play a game
+            </main>
+        </>
+    );
+}
