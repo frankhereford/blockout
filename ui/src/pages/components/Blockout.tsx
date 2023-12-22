b4ae8fa1283c0c49f9597837b0c57e5f137b4b6f
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 22:19:59 -0600

    Add Camera component and update GroundPlane component

--- ui/src/pages/components/Blockout.tsx
+++ ui/src/pages/components/Blockout.tsx
@@ -1,5 +1,7 @@
 import { GroundPlane } from "~/pages/components/GroundPlane"
 import { Lighting } from "~/pages/components/lighting/Lighting";
+import { Camera } from "~/pages/components/Camera";
+import { Vector2 } from 'three';
 
 interface SceneProps {
     width: number;
@@ -10,7 +12,15 @@ interface SceneProps {
 export const Blockout = ({ width, height, depth }: SceneProps) => {
     return (
         <>
-            <GroundPlane width={width} depth={depth} texture="/textures/stones_texture.jpg" />
+            <Camera width={width} height={height} depth={depth} />
+            <GroundPlane
+                width={width}
+                depth={depth}
+                texture="/textures/metal_texture.png"
+                // bumpMap="/textures/stones_bump.jpg"
+                // displacementMap="/textures/stones_displacement.jpg"
+                textureRepeat={new Vector2(4, 4)}
+            />
             <Lighting width={width} height={height} depth={depth} />
         </>
     )
--- ui/src/pages/components/GroundPlane.tsx
+++ ui/src/pages/components/GroundPlane.tsx
@@ -1,20 +1,30 @@
 import { Plane } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber';
-import { TextureLoader } from 'three';
+import { TextureLoader, Vector2, RepeatWrapping } from 'three';
 
 interface GroundPlaneProps {
     width: number;
     depth: number;
     scaleFactor?: number;
-    texture?: string; // New prop for texture file path
+    texture?: string;
+    bumpMap?: string;
+    displacementMap?: string;
+    textureRepeat?: Vector2; // New prop for texture repeat
 }
 
-export function GroundPlane({ width, depth, scaleFactor = 10, texture }: GroundPlaneProps) {
+export function GroundPlane({ width, depth, scaleFactor = 10, texture, bumpMap, displacementMap, textureRepeat = new Vector2(1, 1) }: GroundPlaneProps) {
     const textureMap = texture ? useLoader(TextureLoader, texture) : undefined;
+    const bumpMapTexture = bumpMap ? useLoader(TextureLoader, bumpMap) : undefined;
+    const displacementMapTexture = displacementMap ? useLoader(TextureLoader, displacementMap) : undefined;
+
+    if (textureMap) {
+        textureMap.wrapS = textureMap.wrapT = RepeatWrapping;
+        textureMap.repeat = textureRepeat;
+    }
 
     return (
         <Plane args={[width * scaleFactor, depth * scaleFactor]} rotation={[-Math.PI / 2, 0, 0]} position={[width / 2, -1, depth / 2]} receiveShadow>
-            <meshStandardMaterial attach="material" color="green" map={textureMap} />
+            <meshStandardMaterial attach="material" map={textureMap} bumpMap={bumpMapTexture} displacementMap={displacementMapTexture} displacementScale={1} />
         </Plane>
     );
 }
