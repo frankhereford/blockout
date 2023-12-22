cc30f286f40d321c5629771c0e7f8e2216f5d653
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 18:56:48 -0600

    Commented out unused code in Home component

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -7,7 +7,7 @@ import { api } from "~/utils/api";
 
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -19,9 +19,9 @@ export default function Home() {
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#89e3fc] to-[#3a3e89]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
+            {/* <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
+            </p> */}
             <AuthShowcase />
           </div>
         </div>
