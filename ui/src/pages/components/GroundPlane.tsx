5a8e7872d52e6c20be59f822937835b4b75dbd11
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 21:41:56 -0600

    Add texture support to GroundPlane component

--- ui/src/pages/components/GroundPlane.tsx
+++ ui/src/pages/components/GroundPlane.tsx
@@ -1,15 +1,20 @@
 import { Plane } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
 
 interface GroundPlaneProps {
     width: number;
     depth: number;
     scaleFactor?: number;
+    texture?: string; // New prop for texture file path
 }
 
-export function GroundPlane({ width, depth, scaleFactor = 10 }: GroundPlaneProps) {
+export function GroundPlane({ width, depth, scaleFactor = 10, texture }: GroundPlaneProps) {
+    const textureMap = texture ? useLoader(TextureLoader, texture) : undefined;
+
     return (
         <Plane args={[width * scaleFactor, depth * scaleFactor]} rotation={[-Math.PI / 2, 0, 0]} position={[width / 2, -1, depth / 2]} receiveShadow>
-            <meshStandardMaterial attach="material" color="green" />
+            <meshStandardMaterial attach="material" color="green" map={textureMap} />
         </Plane>
     );
 }
