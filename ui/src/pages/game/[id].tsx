8b387745a88814c6cee219f04b66975246ddebbc
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 20:55:29 -0600

    Refactor Game component to include a scene div

--- ui/src/pages/game/[id].tsx
+++ ui/src/pages/game/[id].tsx
@@ -12,9 +12,11 @@ export default function Game() {
     return (
         <>
             <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#89e3fc] to-[#3a3e89]">
-                <Canvas shadows>
-                    <Blockout width={7} height={10} depth={5} />
-                </Canvas>
+                <div className="scene">
+                    <Canvas shadows>
+                        <Blockout width={7} height={10} depth={5} />
+                    </Canvas>
+                </div>
                 <Link href="/game">
                     <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         New Game
