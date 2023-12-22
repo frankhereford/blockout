aeec9cf0c0fd67e859e2308ee0088d3e6a0356be
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 20:50:13 -0600

    Add Blockout component to Game page

--- ui/src/pages/game/[id].tsx
+++ ui/src/pages/game/[id].tsx
@@ -3,15 +3,21 @@ import { useEffect } from "react";
 
 import { api } from "~/utils/api";
 
+import { Blockout } from "~/pages/components/Blockout";
+
+import { Canvas } from '@react-three/fiber'
+
 export default function Game() {
 
     return (
         <>
             <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#89e3fc] to-[#3a3e89]">
-                this is to play a game
+                <Canvas shadows>
+                    <Blockout width={7} height={10} depth={5} />
+                </Canvas>
                 <Link href="/game">
                     <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Go to Game
+                        New Game
                     </button>
                 </Link>
             </main>
