72e62d8eb6c5dda7be8a1594038edd54b0ea53bb
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-21 20:50:08 -0600

    Add GroundPlane component

---
+++ ui/src/pages/components/GroundPlane.tsx
@@ -0,0 +1,15 @@
+import { Plane } from '@react-three/drei';
+
+interface GroundPlaneProps {
+    width: number;
+    depth: number;
+    scaleFactor?: number;
+}
+
+export function GroundPlane({ width, depth, scaleFactor = 10 }: GroundPlaneProps) {
+    return (
+        <Plane args={[width * scaleFactor, depth * scaleFactor]} rotation={[-Math.PI / 2, 0, 0]} position={[width / 2, -1, depth / 2]} receiveShadow>
+            <meshStandardMaterial attach="material" color="green" />
+        </Plane>
+    );
+}
