2515beb4a56b3129b014cf0a43fb14d6f30de528
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-28 16:16:04 -0600

    Fix formatting in seed.js file

--- ui/prisma/seed.js
+++ ui/prisma/seed.js
@@ -1,28 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
     await prisma.library.create({
         data: {
-            name: 'solo',
+            name: "solo",
             origin: { x: 0, y: 0, z: 0 },
             shape: [{ x: 0, y: 0, z: 0 }],
-            color: 'red',
+            color: "red",
         },
     });
     await prisma.library.create({
         data: {
-            name: 'el',
+            name: "el",
             origin: { x: 1, y: 0, z: 0 },
             shape: [
                 { x: 0, y: 0, z: 0 },
                 { x: 1, y: 0, z: 0 },
                 { x: 2, y: 0, z: 0 },
             ],
-            color: 'green',
+            color: "green",
         },
     });
     await prisma.library.create({
