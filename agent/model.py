530313c0ad4193c628d2706e8d0229818faa3562
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-30 00:29:51 -0600

    Refactor QTrainer's train_step method to include state, action, reward, next_state, and done as tensors

--- agent/model.py
+++ agent/model.py
@@ -33,7 +33,34 @@ class QTrainer:
         self.criterion = nn.MSELoss()
 
     def train_step(self, state, action, reward, next_state, done):
-        pass
-        # state = torch.tensor(state, dtype=torch.float)
-        # next_state = torch.tensor(next_state, dtype=torch.float)
-        # action = torch.tensor(action, dtype=torch.long)
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        if len(state.shape) == 1:
+            # print("Reshaping state")
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done,)
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(
+                    self.model(next_state[idx])
+                )
+
+            # target[idx][torch.argmax(action[idx]).item()] = Q_new
+            target[idx][torch.argmax(action).item()] = Q_new
+            self.optimizer.zero_grad()
+            loss = self.criterion(target, pred)
+            loss.backward()
+            self.optimizer.step()
