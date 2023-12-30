b66560915ea181866afba43a51951e1af6a68116
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-30 11:03:09 -0600

    Add conditional plot call in train function

--- agent/agent.py
+++ agent/agent.py
@@ -189,7 +189,8 @@ def train():
             total_score += score
             mean_score = total_score / agent.number_games
             plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            if agent.number_games % 5 == 0:
+                plot(plot_scores, plot_mean_scores)
 
 
 if __name__ == "__main__":
