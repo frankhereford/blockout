afb700a6b56b5da9e5e71fd53fcc7838faaf8c61
Frank Hereford <frankhereford@users.noreply.github.com>
2023-12-29 21:56:30 -0600

    Add Agent class and import statements

--- agent/agent.py
+++ agent/agent.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
-print("do you want to play a game?")
-
 import torch
 import random
 import numpy as numpy
+from collections import deque
 from TetrisGame import Tetris
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+
+class Agent:
+    def __init__(self):
+        pass
